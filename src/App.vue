9ca2b12b7dd55b836e8288a0ad51d181ab0afbf1
mishael-codes <enyimishael22@gmail.com>
2023-12-06 18:16:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="flex items-center justify-center flex-col p-4">
+    <h1 class="text-amber-400 text-4xl font-semibold">CheckBook</h1>
+    <div class="mt-9">
+      <input v-model="newTodo" placeholder="Add a new todo" class="p-2 rounded-md my-3 bg-amber-200 text-black focus:outline-0 focus:border-amber-100 focus:border-2" />
+      <button @click="addTodo" class="bg-amber-400 p-2 rounded-md">Add Todo</button>
+      <ul>
+        <Todo class="bg-amber-300 p-4 rounded-md" v-for="todo in todos" :key="todo.id" :todo="todo" @edit="editTodo"
+          @delete="deleteTodo" @save="saveTodo" />
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from './components/todo.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
+library.add(faEdit, faTrash);
+
+export default {
+  components: {
+    Todo,
+    FontAwesomeIcon,
+  },
+  data() {
+    return {
+      todos: [],
+      newTodo: '',
+    };
+  },
+  methods: {
+    addTodo() {
+      if (this.newTodo.trim() !== '') {
+        this.todos.push({ id: Date.now(), text: this.newTodo, editing: false });
+        this.newTodo = '';
+      }else{
+        alert("Please enter a valid todo");
+      }
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    editTodo(todoId) {
+      const todo = this.todos.find((t) => t.id === todoId);
+      if (todo) {
+        todo.editing = true;
+      }
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+
+    },
+    deleteTodo(todoId) {
+      this.todos = this.todos.filter((t) => t.id !== todoId);
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+    },
+    saveTodo() {
+      localStorage.setItem('todos', JSON.stringify(this.todos));
+      return `<div><p>Edited Successfully</p></div>`
+    }
+  },
+
+  mounted() {
+    const todos = localStorage.getItem('todos');
+    if (todos) {
+      this.todos = JSON.parse(todos);
+    }
+  },
+};
+</script>
+
+<style>
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+li {
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/components/todo.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="bg-white my-5">
+    <li v-if="todo.editing == false">
+      <p class="mb-3">{{ todo.text }}</p>
+      <hr class="w-full h-1 my-3 border-0 rounded dark:bg-[#090909]">
+      <div>
+        <button @click="editTodo" class="bg-[#090909] w-fit text-amber-300 rounded-md mr-3 py-1 px-3 font-semibold">Edit
+          {{ editBtn }}</button>
+        <button @click="deleteTodo" class="bg-red-800 w-fit text-white rounded-md py-1 px-3 font-semibold">Delete</button>
+      </div>
+      <!-- <FontAwesomeIcon icon="fa-trash" @click="deleteTodo" /> -->
+    </li>
+    <li v-else>
+      <input type="text" v-model="editedText"
+        class="p-1 bg-amber-200 rounded-md focus:outline-0 focus:border-amber-100 focus:border-4">
+      <hr class="w-full h-1 my-3 border-0 rounded dark:bg-[#090909]">
+      <div>
+        <button @click="saveTodo"
+          class="bg-[#090909] w-fit text-amber-300 rounded-md mr-3 py-1 px-3 font-semibold">Save</button>
+        <button @click="deleteTodo" class="bg-red-800 w-fit text-white rounded-md py-1 px-3 font-semibold">Delete</button>
+      </div>
+    </li>
+  </div>
+</template>
+
+<script>
+import editBtn from '../icons/edit.svg';
+
+export default {
+  props: ['todo'],
+  data() {
+    return {
+      editedText: this.todo.text
+    }
+  },
+  methods: {
+    editTodo() {
+      this.$emit('edit', this.todo.id);
+    },
+    deleteTodo() {
+      this.$emit('delete', this.todo.id);
+    },
+    saveTodo() {
+      this.$emit('save', this.todo.id, this.editedText);
+      this.todo.text = this.editedText;
+      this.todo.editing = false;
+    }
+    },
+  };
+</script>
